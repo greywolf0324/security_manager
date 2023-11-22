aee891961160c52c9ae6e18d65f2812bbc31afbb
greywolf0324 <johngrey0324@gmail.com>
2023-11-22 04:55:10 +0100

    with again

--- security/views.py
+++ security/views.py
@@ -152,10 +152,10 @@ def history(request):
 
   file_contents = []
   for history in new_histories:
-    f = open((Path(__file__).resolve().parent.parent / history.output), 'rb')
-    # with open((Path(__file__).resolve().parent.parent / history.output), 'rb') as f:
-    temp = f.read()
-    file_contents.append(f.read())
+    # f = open((Path(__file__).resolve().parent.parent.parent / history.output), 'rb')
+    with open((Path(__file__).resolve().parent.parent.parent / history.output), 'rb') as f:
+      temp = f.read()
+      file_contents.append(f.read())
 
   [customer_names, PO_dates] = Output_analyzer(file_contents)
 
