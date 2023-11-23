fe35143bddc4f30459b999b22574dff77ece8fe7
greywolf0324 <johngrey0324@gmail.com>
2023-11-23 02:29:21 +0100

    reverse error fixing

--- security/views.py
+++ security/views.py
@@ -163,20 +163,20 @@ def history(request):
     # f = open((Path(__file__).resolve().parent.parent.parent / history.output), 'rb')
     with open(Path(__file__).resolve().parent.parent / history.output, 'rb') as f:
       file_contents.append(f.read())
-  
-  # if len(file_contents) == 0:
-  #   histories = "empty"
+  print(file_contents)
+  if len(file_contents) == 0:
+    histories = "empty"
 
-  # else:
-  #   [customer_names, PO_dates] = Output_analyzer(file_contents)
+  else:
+    [customer_names, PO_dates] = Output_analyzer(file_contents)
 
-  #   users = []
-  #   for history in new_histories:
-  #     users.append(str(history.user).split("@")[0])
-  #   # print(customer_names, users, creation_dates, PO_dates, type(PO_dates[0]))
-  #   # histories = zip(customer_names, users, creation_dates, PO_dates)
+    users = []
+    for history in new_histories:
+      users.append(str(history.user).split("@")[0])
+    # print(customer_names, users, creation_dates, PO_dates, type(PO_dates[0]))
+    # histories = zip(customer_names, users, creation_dates, PO_dates)
     
-  #   histories = zip(customer_names, users, creation_dates, PO_dates)
+    histories = zip(customer_names, users, creation_dates, PO_dates)
   
   return render(request, 'history.html', {"histories": histories, "real": paths, "history": history_location, "new": file_contents})
   
