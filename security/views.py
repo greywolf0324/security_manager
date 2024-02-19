410c3f0db894a8a8171266cd4ad83842e855a7d6
greywolf0324 <johngrey0324@gmail.com>
2024-02-19 03:24:12 -0500

    oms_fetcher avoid in home page initialize

--- security/views.py
+++ security/views.py
@@ -46,7 +46,7 @@ customer_list.sort()
 @login_required
 def home(request):
   user = request.user
-  generator.mondayoms_fetcher()
+  # generator.mondayoms_fetcher()
   if request.method == 'POST':
     files = request.FILES.getlist('data')
     matching_res = json.loads(request.POST['input'])
